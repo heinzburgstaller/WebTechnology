8e243fc5e93d325765157b4370ee70a86329c714
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2017-10-31 13:14:31 +0100

    Added PeerJS

--- ocean-storm/src/app/app.component.ts
+++ ocean-storm/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  peer;
+  peerId;
+
+  constructor() {
+    this.peer = new Peer({ key: 'bzwelzt8iihn0zfr' });
+
+    this.peer.on('open', function (id) {
+      this.peerId = id;
+      console.log('My peer ID is: ' + this.peerId);
+    });
+  }
+
 }
