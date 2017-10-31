461b6efa09d7127cd8d37133cdd54226afc7c957
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2017-10-31 12:41:54 +0100

    Added bootstrap

--- ocean-storm/src/app/app.module.ts
+++ ocean-storm/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
