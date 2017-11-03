2207b3f6401cad0980034dfb7fdce8349622c4d9
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2017-11-03 15:22:09 +0100

    Net

--- ocean-storm/src/app/network.service.ts
+++ ocean-storm/src/app/network.service.ts
@@ -27,21 +27,44 @@ export class NetworkService {
       this.addItem('PlayerX', id, false);
     });
 
+    this.peer.on('error', (error) => {
+      if (error.type === 'peer-unavailable') {
+        const n = error.message.split(' ');
+        const unPeerId = n[n.length - 1];
+        this.deleteItem(unPeerId);
+      }
+    });
+
     TimerObservable
-      .create(2500, 1000).subscribe(() => {
-        console.log(this.peerId);
+      .create(2500, 250).subscribe(() => {
+        this.players.subscribe(arr => {
+          const randomIndex = Math.floor(Math.random() * (arr.length - 1));
+          const conn = this.peer.connect(arr[randomIndex].peerId);
+        });
       });
   }
 
   addItem(name: string, peerId: any, isPlaying: boolean) {
     this.playersRef.push({ name: name, peerId: peerId, isPlaying: isPlaying });
   }
+
   updateItem(key: string, name: string, isPlaying: boolean) {
     this.playersRef.update(key, { name: name, isPlaying: isPlaying });
   }
-  deleteItem(key: string) {
-    this.playersRef.remove(key);
+
+  deleteItem(peerId: string) {
+    if (this.peerId === peerId) {
+      return;
+    }
+
+    this.players.subscribe(arr => {
+      const x = arr.filter(
+        player => player.peerId === peerId);
+      const key = x[0].key;
+      this.playersRef.remove(key);
+    });
   }
+
   deleteEverything() {
     this.playersRef.remove();
   }
