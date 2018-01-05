e7d15ab687b1e992c3e9523be7d9a773df0b4870
Heinz Burgstaller <heinz.burgstaller@gmail.com>
2018-01-05 13:10:54 +0100

    Fix

--- ocean-storm/src/app/network.service.ts
+++ ocean-storm/src/app/network.service.ts
@@ -43,7 +43,7 @@ export class NetworkService {
     });
 
     this.peer.on('connection', conn => {
-      if (this.connectedTo !== null) {
+      if (this.enemyPeerId !== null) {
         return;
       }
 
