f38af570175b03b3a2f4e846de86bf2ecbe4d1e6
Klemens Strasser <klemens.strasser@gmail.com>
2018-01-06 18:38:56 +0100

    Fixed a typo

--- ocean-storm/src/app/models/Placeholder.ts
+++ ocean-storm/src/app/models/Placeholder.ts
@@ -1,7 +1,7 @@
 enum Placeholder {
   standard = 'Choose an opponent on the left side and press \'Play\'.',
   win = 'You are the champ, destroy another opponent!',
-  loose = 'I\'n not sure your oppenent didn\'t cheat, usually you win ;-).'
+  loose = 'I\'m not sure your oppenent didn\'t cheat, usually you win ;-).'
 }
 
 
