1f72749d2cccf5229f955e8351ca1c4b7f8529d2
Klemens Strasser <klemens.strasser@gmail.com>
2018-01-07 16:14:07 +0100

    Fixed the placeholder text

--- ocean-storm/src/app/models/Placeholder.ts
+++ ocean-storm/src/app/models/Placeholder.ts
@@ -1,5 +1,5 @@
 enum Placeholder {
-  standard = 'Choose an opponent on the left side and press \'Play\'.',
+  standard = 'Drag an opponent to the drop zone to start a game.',
   win = 'You are the champ, destroy another opponent!',
   loose = 'I\'m not sure your oppenent didn\'t cheat, usually you win ;-).'
 }
